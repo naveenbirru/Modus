0702c24339c462523921d7f8fcf4a3709b1f8bb7
nilu23 <nilu23@gmail.com>
2018-01-07 15:50:22 -0800

    Delete items

--- app/src/main/java/com/example/thor/modus/ItemList.java
+++ app/src/main/java/com/example/thor/modus/ItemList.java
@@ -6,6 +6,9 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -58,9 +61,44 @@ public class ItemList extends BaseActivity {
                 launchFoodItemDetialActivity(adapterView, MainActivity.FragementToLaunch.VIEW, position);
             }
         });
+
+        registerForContextMenu(listView);
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
 
+        MenuInflater menuInflater = this.getMenuInflater();
+        menuInflater.inflate(R.menu.long_press_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        FoodItem foodItem = (FoodItem) this.foodItemAdapter.getItem(info.position);
+
+        switch(item.getItemId()) {
+            case R.id.edit:
+                Log.d("Menu Clicks", "we pressed edit");
+                launchFoodItemDetialActivity(MainActivity.FragementToLaunch.EDIT, info.position);
+                return true;
+            case R.id.delete:
+                FoodDbAdapter dbAdapter = new FoodDbAdapter(this.getBaseContext());
+                dbAdapter.open();
+
+                dbAdapter.deleteFoodItem(foodItem.getID());
+
+                foodItems.clear();
+
+                //NAVEEN to update the array adapter
+                foodItems.addAll(dbAdapter.getAllFoodItems());
+                foodItemAdapter.notifyDataSetChanged();
+                dbAdapter.close();
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
 
     public void createItemInDB(Context ctxt, String title, String weight, FoodItem.Category category, String expiryDate, long ID, Boolean newNote){
         FoodDbAdapter foodDbAdapter = new FoodDbAdapter(ctxt);
@@ -82,9 +120,33 @@ public class ItemList extends BaseActivity {
         foodDbAdapter.close();
     }
 
-    private void launchFoodItemDetialActivity(AdapterView<?> adapterView, MainActivity.FragementToLaunch ftl, int position) {
+    private void launchFoodItemDetialActivity( AdapterView<?> adapterView,MainActivity.FragementToLaunch ftl, int position) {
 
         FoodItem foodItem = (FoodItem)adapterView.getItemAtPosition(position) ;
+        Intent intent = new Intent(this, FoodItemDetailActivity.class);
+
+        intent.putExtra(MainActivity.FOOD_ITEM_TITLE_EXTRA, foodItem.getTitle());
+        intent.putExtra(MainActivity.FOOD_ITEM_WEIGHT_EXTRA, foodItem.getWeight());
+        intent.putExtra(MainActivity.FOOD_ITEM_ID_EXTRA, foodItem.getID());
+        intent.putExtra(MainActivity.FOOD_ITEM_DATE_EXTRA, foodItem.getExpiryDate());
+        intent.putExtra(MainActivity.FOOD_ITEM_CATEGORY_EXTRA, foodItem.getCategory());
+
+        switch(ftl) {
+            case EDIT:
+                intent.putExtra(MainActivity.FOOD_ITEM_FRAGMENT_TO_LOAD_EXTRA, MainActivity.FragementToLaunch.EDIT);
+                break;
+            case VIEW:
+                intent.putExtra(MainActivity.FOOD_ITEM_FRAGMENT_TO_LOAD_EXTRA, MainActivity.FragementToLaunch.VIEW);
+                break;
+        }
+
+        startActivity(intent);
+
+    }
+
+    private void launchFoodItemDetialActivity(MainActivity.FragementToLaunch ftl, int position) {
+
+        FoodItem foodItem = (FoodItem) foodItemAdapter.getItem(position) ;
         Intent intent = new Intent(this, FoodItemDetailActivity.class);
 
         intent.putExtra(MainActivity.FOOD_ITEM_TITLE_EXTRA, foodItem.getTitle());
